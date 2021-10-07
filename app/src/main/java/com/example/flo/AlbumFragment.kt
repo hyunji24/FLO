9da435ac54d7b7484debe288226d508c477ce46f
hyunji <24hyunji@naver.com>
2021-10-07 20:37:46 +0900

    viewpager, tablayout 추가 중

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
 
     lateinit var binding:FragmentAlbumBinding
+    var toggleIsOff:Boolean=true
+    val information=arrayListOf("수록곡","상세정보","영상")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,7 +22,6 @@ class AlbumFragment : Fragment() {
     ): View? {
         binding= FragmentAlbumBinding.inflate(inflater,container,false)
 
-       //binding.albumPlayIv.bringToFront()
 
         binding.albumArrowIb.setOnClickListener{
             (context as MainActivity).supportFragmentManager.beginTransaction()
@@ -27,10 +29,37 @@ class AlbumFragment : Fragment() {
                 .commitAllowingStateLoss() //나중에 학습ㄱ
         }
 
-        //binding.songLilacLayout.setOnClickListener{
-        //    Toast.makeText(activity,"라일락",Toast.LENGTH_SHORT).show()
-        //}
+//        binding.albumToggleoffIv.setOnClickListener {
+//            setToggleStatus(toggleIsOff)
+//        }
+//
+//
+//
+//        binding.songLilacLayout.setOnClickListener{
+//            Toast.makeText(activity,"라일락",Toast.LENGTH_SHORT).show()
+//        }
+        val albumAdapter=AlbumViewpagerAdapter(this)
+        binding.albumContentVp.adapter=albumAdapter
+
+        TabLayoutMediator(binding.tabLayout,binding.albumContentVp){
+            tab,position ->
+            tab.text=information[position]
+        }.attach() //viewpager랑 tablayout붙이기
+
 
         return binding.root
     }
+
+//    fun setToggleStatus(toggleOff:Boolean){
+//        if(toggleOff) {
+//            binding.albumToggleoffIv.setImageResource(R.drawable.btn_toggle_on)
+//            toggleIsOff=false
+//
+//        }
+//        else {
+//            binding.albumToggleoffIv.setImageResource(R.drawable.btn_toggle_off)
+//            toggleIsOff=true
+//        }
+//
+//    }
 }
--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -6,12 +6,15 @@ import android.os.PersistableBundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.flo.databinding.ActivityMainBinding
 import com.example.flo.databinding.ActivitySongBinding
 
 class SongActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySongBinding
+    var isitRandom:Boolean=false
+    var repeatStatus:Int=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,8 @@ class SongActivity : AppCompatActivity() {
 
         //window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         //MAINLAYOUT.setPadding(0, statusBarHeight(this), 0, 0)
+
+
 
 
         if(intent.hasExtra("title")&& intent.hasExtra("singer")){
@@ -38,8 +43,51 @@ class SongActivity : AppCompatActivity() {
            setPlayerStatus(true)
         }
 
+        binding.songRandomIv.setOnClickListener {
+            setRandomStatus(isitRandom)
+        }
+
+       binding.songRepeatIv.setOnClickListener {
+            setRepeatStatus(repeatStatus)
+        }
+
+
+
 
     }
+
+
+    fun setRandomStatus(isRandom:Boolean){
+        if(!isRandom) {
+            binding.songRandomIv.setImageResource(R.drawable.btn_player_random_on_light)
+            isitRandom=true
+
+        }
+        else {
+            binding.songRandomIv.setImageResource(R.drawable.nugu_btn_random_inactive)
+            isitRandom=false
+        }
+
+        }
+
+    @JvmName("setRepeatStatus1")
+    fun setRepeatStatus(status:Int){
+        if(status==0){
+            binding.songRepeatIv.setImageResource(R.drawable.btn_player_repeat_on_light)
+            repeatStatus=1
+        }
+        else if(status==1){
+            binding.songRepeatIv.setImageResource(R.drawable.btn_player_repeat_on1_light)
+            repeatStatus=2
+        }
+        else{
+            binding.songRepeatIv.setImageResource(R.drawable.nugu_btn_repeat_inactive)
+            repeatStatus=0
+        }
+    }
+
+
+
 
     fun setPlayerStatus(isPlaying:Boolean){
         if(isPlaying){
